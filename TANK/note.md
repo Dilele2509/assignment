571d1b6c38b00ebd412e365793e09e6baa9c1ac5
Dilele <levy3443@gmail.com>
2021-11-30 01:33:15 +0700

    upload assignment file

---
+++ TANK/note.md
@@ -0,0 +1,17 @@
+# Giải thích code JS:
+
+**Tổng quan**
+* khi cho ảnh tank ban đầu có property là position mang giá trị relative và top left ban đầu đều là 0px (chỉnh trong file CSS). Lúc đó tọa độ left và top của hình (có thể xem như tọa độ x và y) đều có giá trị là 9 (in ra màn hình console để xem).
+* Xét giới hạn border trong thẻ div được thiết lập (xem trong file CSS) là hình vuông có độ lớn các cạnh 190px. 
+* Khi in tọa độ của tank ra console ta sẽ thấy tank có tọa độ (9,9) khi top và left bằng 0px.
+* Tức là khi top/left tăng lên 10px thì tọa độ của tank sẽ tăng từ 9 thành 28 (tức là 19 đơn vị). Tương tự, ví dụ tank đang có tọa độ x là 47 khi left bằng 38px, để quay về tọa độ 28 ta phải giảm đi 19px (tức là left = 19px ).
+* Từ đó suy ra để tank nằm hoàn toàn trong border thì tọa độ tối đa của tank là 180 và tối thiểu là 9, tương ứng với max top/left = 171px và min là 0px. (đối với boom cũng tương tự).
+
+**Chi tiết**
+* *khi di chuyển sang phía bên trái:* ta phải thay đổi giá trị của left giảm đi bằng cách lấy tọa độ x của tank trừ đi 28. Vì tọa độ (x,y) của tank và left/top luôn chênh lệch nhau 9 lần. VD: left= 0px -> x=9, left=19 -> x=28, left=38 -> x= 47.
+* Nên nếu ta trừ đi 19 thay vì 28 thì tank chỉ di chuyển 10 đơn vị trong mỗi lần di chuyển thay vì 19. Điều này sẽ dẫn dến việc tank có thể đi ra khỏi border.
+>tank.style.left = curPosition.left - 28 + "px";
+* *khi di chuyển sang phía bên phải:* Khác với di chuyển sang trái, khi di chuyển sang phải thay vì cộng cho 28 thì ta lại phải cộng với 10. Lý do của sự khác biệt này là do chúng ta phải tính luôn cả phần kích thước của hình rồi tính toán giá trị cộng vào hợp lý để tank di chuyển đúng 19 đơn vị trong mỗi lần di chuyển.
+* *Những điều này cũng xảy ra tương tự với di chuyển lên và xuống:* khi di chuyển đi lên ta thực hiện giống với di chuyển sang phải nhưng thay đổi giá trị top và left với nhau, và di chuyển xuống tương tự với di chuyển qua phải.
+* *Đặt điều kiện di chuyển:* để tank không chạy ra ngoài border ta phải đặt điều kiện dựa trên các thông số min và max. VD:
+>if(curPosition.left < 180)
---
+++ TANK/tank_main.js
@@ -0,0 +1,78 @@
+const tank = document.getElementById("tank");   
+const boom = document.getElementById("boom");
+
+function randBoom(){    /* lay gia tri ngau nhien cho toa do cua Boom */
+    let topBoom = Math.floor(Math.random()*(180 - 9));
+    let leftBoom = Math.floor(Math.random()*(180 - 9));
+    boom.style.top = topBoom + "px";    
+    boom.style.left = leftBoom + "px";
+}
+function Position(ele){ /* lay gia tri toa do hien tai cua Element (tank hoac boom) */
+    return{
+        left: ele.offsetLeft,
+        top: ele.offsetTop
+    }
+}
+function Move(event){
+    let curPosition = Position(tank);
+    let boomPosition = Position(boom);
+
+    switch(event.target.id){
+        case "leftButt":
+            tank.style.transform = "rotate(270deg)";    /* xoay hinh */
+            if(curPosition.left > 9){
+                tank.style.left = curPosition.left - 28 + "px";
+                console.log(curPosition.left-19); /* in ra man hinh console toa do x hien tai */
+                if(curPosition.left - 28 < boomPosition.left){
+                    /* dat dieu kien cho toa do left cua tank giao voi left cua boom */
+                    if((curPosition.top >= (boomPosition.top - 19)) && (curPosition.top <= (boomPosition.top + 19))){
+                        /* dat dieu kien khi toa do top cua tank nam trong 1 khoang +- cua toa do top cua boom */
+                        tank.src = "./img/explosion.png";
+                        alert("GAME OVER!");
+                    }
+                }
+            }    
+            break;
+
+        case "rightButt":
+            tank.style.transform = "rotate(90deg)";
+            if(curPosition.left < 180){
+                tank.style.left = curPosition.left + 10 + "px";
+                console.log(curPosition.left+19);
+                if(curPosition.left + 37 > boomPosition.left){
+                    if((curPosition.top >= (boomPosition.top - 19)) && (curPosition.top <= (boomPosition.top + 19))){
+                        tank.src = "./img/explosion.png";
+                        alert("GAME OVER!");
+                    }
+                }
+            }
+            break;
+
+        case "upButt":
+            tank.style.transform = "rotate(0deg)";
+            if(curPosition.top > 9){
+                tank.style.top = curPosition.top - 28 + "px";
+                console.log(curPosition.top-19);
+                if(curPosition.top - 28 > boomPosition.top){
+                    if((curPosition.left >= (boomPosition.left - 19)) && (curPosition.left <= (boomPosition.left + 19))){
+                        tank.src = "./img/explosion.png";
+                        alert("GAME OVER!");
+                    }
+                }
+            }
+            break;
+        
+        case "downButt":
+            tank.style.transform = "rotate(180deg)";
+            if(curPosition.top < 180){
+                tank.style.top = curPosition.top + 10 +"px";
+                console.log(curPosition.top+19);
+                if(curPosition.top + 37 > boomPosition.top){
+                    if((curPosition.left >= (boomPosition.left - 19)) && (curPosition.left <= (boomPosition.left + 19))){
+                        tank.src = "./img/explosion.png";
+                        alert("GAME OVER!");
+                    }
+            }
+            }
+    }
+}
